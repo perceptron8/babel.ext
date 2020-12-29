0c16aa8ecb35a242f0fbfb92ca336e1ee1ebafa0
perceptron8 <perceptron8@users.noreply.github.com>
2020-12-29 11:49:30 +0100

    transictional update

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 "use strict";
 
-const swmmo = document.getElementById("SWMMO");
-if (swmmo) {
-	const clone = swmmo.cloneNode();
-	const lang = chrome.i18n.getMessage("lang");
-	clone.setAttribute("flashvars", swmmo.getAttribute("flashvars")
-		.replace(/lang=[a-z]{2}[-][a-z]{2}/g, "lang=" + lang));
-	swmmo.parentNode.replaceChild(clone, swmmo);
-}
+const lang = chrome.i18n.getMessage("lang");
+const realmLang = chrome.i18n.getMessage("realmLang");
+
+const script = document.createElement("script");
+script.appendChild(document.createTextNode(`
+	window.gameClientStartUrl = window.gameClientStartUrl?.replace(/lang=[a-z]{2}[-][a-z]{2}/g, "lang=${lang}");
+	window.gameClientStartUrl = window.gameClientStartUrl?.replace(/realmLang=[a-z]{2}/g, "realmLang=${realmLang}");
+`));
+document.body.appendChild(script);
